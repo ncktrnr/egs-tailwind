2ab2c1a5fcb8cce6183efad450caccc3946a34d4
ncktrnr <>
2023-09-11 00:21:06 +0100

    initial pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,43 @@ module.exports = {
     './layouts/*.html'
   ],
   theme: {
-    extend: {},
+    container: { center: true,
+      padding: {
+      DEFAULT: '0.5rem', sm: '1rem',
+      lg: '1.5rem',
+      xl: '2rem'
+      }, 
+    },
+
+    extend: {
+      fontFamily: { 
+        headline: ['Merriweather'],
+        'sans': ['Lato'],
+        'light': ['LatoLight'],
+      },
+      colors: {
+        'gold': '#fdca40',
+        'aths': '#d6cfb2',
+        'light-bg': '#f8f7f3',
+        'dark-engine': '#3c3e42',
+        'baragon': "#591903",
+        'bakers': '#5f3400',
+        'verdun-dark': '#3c4315',
+        'verdun': '#50591c',
+        'verdun-bright': '#78804a',
+
+        link: {
+          "text": "#0d77b5",
+          "hover": "#50591c",
+          "visited": "#51758B"
+        }
+      }, 
+    },
   },
+
   plugins: [
     require('@tailwindcss/typography'),
+    require('tailwindcss-debug-screens'),
   ],
 }
 
